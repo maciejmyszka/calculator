a190b52ea8c171d47cbac52e62bf4af86de6de83
Maciej Myszka <maciejmyszka99@gmail.com>
2023-08-21 20:44:22 +0200

    Added README

--- src/components/TableFoot/index.tsx
+++ src/components/TableFoot/index.tsx
@@ -2,19 +2,17 @@ import { Td, Tfoot, Tr } from '@chakra-ui/react';
 import { PriceDisplay } from '../PriceDisplay';
 import { DiscountForm } from '../DiscountForm';
 
-export const TableFoot = () => {
-  return (
-    <Tfoot>
-      <Tr>
-        <Td></Td>
-        <Td></Td>
-        <Td>
-          <DiscountForm />
-        </Td>
-        <Td>
-          <PriceDisplay />
-        </Td>
-      </Tr>
-    </Tfoot>
-  );
-};
+export const TableFoot = () => (
+  <Tfoot>
+    <Tr>
+      <Td></Td>
+      <Td></Td>
+      <Td>
+        <DiscountForm />
+      </Td>
+      <Td>
+        <PriceDisplay />
+      </Td>
+    </Tr>
+  </Tfoot>
+);
